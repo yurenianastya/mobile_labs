cd8912b9582b83d9de428d7b34e09222dd676bf1
yurenianastya <nastyayurenia@gmail.com>
2020-12-11 16:35:33 +0200

    adding 1 lab

---
+++ App.js
@@ -0,0 +1,58 @@
+// App.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './components/login';
+import Signup from './components/signup';
+import Dashboard from './components/dashboard';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+    return (
+        <Stack.Navigator
+            initialRouteName="Signup"
+            screenOptions={{
+                headerTitleAlign: 'center',
+                headerStyle: {
+                    backgroundColor: '#000',
+                },
+                headerTintColor: '#FF69B4',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            }}>
+            <Stack.Screen
+                name="Signup"
+                component={Signup}
+                options={{ title: 'Sign up' }}
+            />
+            <Stack.Screen
+                name="Login"
+                component={Login}
+                options={
+                    {title: 'Login'},
+                {headerLeft: null}
+                }
+            />
+            <Stack.Screen
+                name="Dashboard"
+                component={Dashboard}
+                options={
+                    { title: 'Dashboard' },
+                {headerLeft: null}
+                }
+            />
+        </Stack.Navigator>
+    );
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
+    );
+}
